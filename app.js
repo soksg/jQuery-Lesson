5655647cf74ca5728f8a2637d55e02655cfddb9c
EC2 Default User <sk2361ksg@gmail.com>
2023-01-11 04:59:45 +0000

    [Add]スライド表示

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#000',
+//     'height': '100px'
+//   });
+// })
+
+// 上から下にスライドさせる（表示する）
+// $(function(){
+//   $('.box1').slideDown();
+// });
+
+// 下から上にスライドさせる（隠す）
 $(function(){
-  $('.box1').css({
-    'background-color': '#000',
-    'height': '100px'
-  });
-})
+  $('.box1').slideUp();
+});
